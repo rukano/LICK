7387305d0f65128d564c380417f76d432260c0ee
Juan Gabriel Alzate Romero <rukano@cheesecake.local>
2011-03-10 19:11:42 +0100

    rectangle test code on main

--- examples/test_project_rukano/main.lua
+++ examples/test_project_rukano/main.lua
@@ -7,7 +7,9 @@ lick.clearFlag = false
 
 rot = rot or 0
 --p = Point(0, 0, ez.color("red", 255), 3, "smooth")
-p = Polygon({100, 100, 200, 200, 100, 300, 400, 500, 234, 432, 123, 321}, ez.color("red"), "line")
+--p = Polygon({100, 100, 200, 200, 100, 300, 400, 500, 234, 432, 123, 321}, ez.color("red"), "line")
+
+p = Rect(100, 100, 400, 300, ez.color("blue"), "fill")
 ez.setBlendMode("alpha")
 
 function love.update(dt)
@@ -18,7 +20,7 @@ function love.draw()
    ez.cls(255)
 
    ez.push()
-   p:draw("line")
+   p:draw("fill")
    
 --[[   for i=1,500 do
       ez.translate(rot/2, rot)
